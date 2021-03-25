10bad096b8bfd9bf7481081d91d67e570c0a1cb5
Aki Nishimura <aki.nishimura@jhu.edu>
2021-03-25 11:14:16 -0400

    R script to define colors

---
+++ R/colors.R
@@ -0,0 +1,22 @@
+tableau_color <- list(
+  blue = '#4E79A7',
+  orange = '#F28E2B',
+  red = '#E15759',
+  light_teal = '#76B7B2',
+  green = '#59A14F',
+  yellow = '#EDC948',
+  purple = '#B07AA1',
+  pink = '#FF9DA7',
+  brown = '#9C755F',
+  light_gray = '#BAB0AC'
+)
+
+jhu_color <- list(
+  heritageBlue = '#002D72', # https://brand.jhu.edu/color/
+  spiritBlue = '#68ACE5',
+  # From the secondary color palette
+  orange = '#CF4520', 
+  maroon = '#76232F',
+  green = '#009B77', 
+  yellow = '#F1C400'
+)
