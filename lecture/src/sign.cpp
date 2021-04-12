43ae9d87e3c5aa72fd2a83a8b352ab2648b6c5cd
Aki Nishimura <aki.nishimura@jhu.edu>
2021-04-12 11:38:45 -0400

    Revert "Add scalar ver as helper to vectorized ones"
    This reverts commit 46e95e8c5a5d49fc638820af345d7b7aee039c48.
    Default Rcpp compilation does not seem to perform inlining.

--- lecture/src/sign.cpp
+++ lecture/src/sign.cpp
@@ -1,38 +1,30 @@
 #include <Rcpp.h>
 using Rcpp::NumericVector;
 
-// [[Rcpp::export]]
-double sign_via_if_scalar(double x) {
-  if (x > 0) {
-    return 1.;
-  } else if (x < 0) {
-    return -1.;
-  } else {
-    return 0.;
-  }
-}
 
 // [[Rcpp::export]]
 NumericVector sign_via_if(NumericVector x) {
   int n = x.size();
   NumericVector x_sign(n);
   for(int i = 0; i < n; ++i) {
-    x_sign[i] = sign_via_if_scalar(x[i]);
+    if (x[i] > 0) {
+      x_sign[i] = 1;
+    } else if (x[i] < 0) {
+      x_sign[i] = -1;
+    } else {
+      x_sign[i] = 0;
+    }
   }
   return x_sign;
 }
 
-// [[Rcpp::export]]
-double sign_via_subtract_scalar(double x) {
-  return (double)(x > 0) - (double)(x < 0);
-}
 
 // [[Rcpp::export]]
 NumericVector sign_via_subtract(NumericVector x) {
   int n = x.size();
   NumericVector x_sign(n);
   for(int i = 0; i < n; ++i) {
-    x_sign[i] = sign_via_subtract_scalar(x[i]);
+    x_sign[i] = (double)(x[i] > 0) - (double)(x[i] < 0);
   }
   return x_sign;
 }
