bb516af9c6a11f117b97325011c32d75cf3dac18
Aki Nishimura <aki.nishimura@jhu.edu>
2021-04-22 17:25:06 -0400

    Get space right in axpy_c

--- lecture/src/axpy_c.cpp
+++ lecture/src/axpy_c.cpp
@@ -4,7 +4,7 @@ using Rcpp::NumericVector;
 // [[Rcpp::export]]
 NumericVector axpy_c(double a, NumericVector x, NumericVector y) {
   int n = x.size();
-  for(int i = 0; i < n; ++i) {
+  for (int i = 0; i < n; ++i) {
     y[i] += a * x[i];
   }
   return y;
