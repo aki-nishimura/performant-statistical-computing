67f16b92e0f36951f1272ef441c75c6dc597070b
Aki Nishimura <aki.nishimura@jhu.edu>
2021-04-22 16:11:00 -0400

    Match spacings in cpp file for axpy using RcppArmadillo

--- lecture/src/axpy_arma.cpp
+++ lecture/src/axpy_arma.cpp
@@ -1,7 +1,9 @@
 #include <RcppArmadillo.h>
 // [[Rcpp::depends(RcppArmadillo)]]
 
+
+
 // [[Rcpp::export]]
 void axpy_arma(double a, arma::colvec& x, arma::colvec& y) {
-  y += a * x;
+	y += a * x;
 }
